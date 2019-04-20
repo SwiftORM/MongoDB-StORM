17c607526258a72ac08a81b6d86ea43c82a93917
boyankov <boyan.yankov@yahoo.com>
2019-04-20 20:58:18 +0300

    updated `Package.swift` - using new syntax (API) for package dependencies

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
 	name: "MongoDBStORM",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "1.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", from: "3.0.0"),
 	]
 )
