e1bce63b72636ecc70ad12263a25aa5bcf32db46
boyankov <boyan.yankov@yahoo.com>
2019-04-20 21:31:26 +0300

    updated `Package.swift` - fixed parameter order in package initialization

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@
 import PackageDescription
 let package = Package(
 	name: "MongoDBStORM",
-	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
 		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "1.0.0"),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", from: "3.0.0"),
-	]
+	],
+	targets: []
 )
