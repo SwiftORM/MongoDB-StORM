ac378efa994ea61b83d27ae9b3150fdcd4bdab28
boyankov <boyan.yankov@yahoo.com>
2019-04-20 21:34:27 +0300

    updated `Package.swift` - updated initialization API used for creating dependency packages

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
 	name: "MongoDBStORM",
 	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "1.0.0"),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+		.package(url: "https://github.com/iamjono/SwiftRandom.git", from: "1.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", from: "3.0.0"),
 	],
 	targets: []
 )
