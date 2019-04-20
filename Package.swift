01b707a9755a31ecc4334caa9b219cbf36764d43
boyankov <boyan.yankov@yahoo.com>
2019-04-20 21:52:11 +0300

    updated `Package.swift` - added products

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@
 import PackageDescription
 let package = Package(
 	name: "MongoDBStORM",
+	products: [
+		.library(name: "MongoDBStORM", targets: ["MongoDBStORM"])
+	],
 	dependencies: [
 		.package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
